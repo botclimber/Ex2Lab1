9716d1bc8a14a1ca1681995865c3c8cfce1fe89a
SeeOrNotToSee <49307104+SeeOrNotToSee@users.noreply.github.com>
2019-04-05 11:28:47 +0100

    Update Clients.cs
    Penso que seja só isto para o cliente

--- Clients.cs
+++ Clients.cs
@@ -8,20 +8,55 @@ namespace Ex2Lab1
 {
     class Clients
     {
-        const int MAX = 20;
-        static int numClients = 0;
-        static Client[] clients = new Client[MAX];
-
-        static Clients()
+        #region ATTRIBUTES
+            string name;
+        int nif, phoneNum;
+            #endregion
+                
+        #region CONSTRUTORES
+                // metodo insere pesso
+                /// <summary>
+                /// Construtor para dados vindos do exterior
+                /// </summary>
+                /// <param name="n">Nome do cliente</param>
+                /// <param name="nif">Numero de contribuinte do cliente</param>
+                /// <param name="phone">Numero de telefone do cliente</param>
+                public Cliente(string n, int nif, int phone){
+            }
+                #endregion
+        #region OVERRIDES 
+                    // metodo verifica se pessoa existe
+         /// <summary>
+        /// Compara dois objectos tipo Cliente
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(Object obj)
         {
-            clients = new Client[MAX];
+            
         }
+                #endregion
+                    
+        
+       
 
-        // metodo verifica espaço
+        
 
-        // metodo verifica se pessoa existe
+        
 
-        // metodo insere pessoa
+        
 
     }
 }
+
+/*const int MAX = 20;
+        static int numClients = 0;
+        static Client[] clients = new Client[MAX];
+        
+        
+        
+         static Clients()
+        {
+            clients = new Client[MAX];
+        }
+*/
